dcc1caa1cb66f8da8bf176dabeba407f607191e7
carlos horacio lopez <krloss@gmail.com>
2022-03-15 21:55:03 -0300

    clase40
    - add Procfile
    - add heroku pluging

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -jar app-web/target/dependency/webapp-runner.jar --port $PORT app-web/target/*.war
---
+++ app-web/src/main/java/ar/com/educacionit/servlet/PruebaDeJuanPablo.java
@@ -0,0 +1,15 @@
+package ar.com.educacionit.servlet;
+
+public class PruebaDeJuanPablo {
+
+	public static void main(String[] args) {
+		// byte < short < int < long < float < double
+		Integer numero1 = 128;//byte
+        Integer numero2 = 128;//byte        
+        String palabra1 = "hola";
+        String pabalabra2 = "hola";
+        System.out.println(numero1.equals(numero2));
+        System.out.println(pabalabra2 == palabra1);
+	}
+
+}
