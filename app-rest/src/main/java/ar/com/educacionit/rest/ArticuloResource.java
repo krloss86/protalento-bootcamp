2180fded827c78e0dc27a2630b6713b062ea4de4
carlos horacio lopez <krloss@gmail.com>
2022-05-10 19:55:57 -0300

    clase53
    - api rest básico: sin capa service

--- app-rest/src/main/java/ar/com/educacionit/rest/ArticuloResource.java
+++ app-rest/src/main/java/ar/com/educacionit/rest/ArticuloResource.java
@@ -1,17 +1,19 @@
 package ar.com.educacionit.rest;
 
-import java.util.Collection;
+//import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-import ar.com.educacionit.domain.Articulos;
-import ar.com.educacionit.services.ArticulosServices;
-import ar.com.educacionit.services.exceptions.ServiceException;
-import ar.com.educacionit.services.impl.ArticulosServicesImpl;
+//import ar.com.educacionit.domain.Articulos;
+//import ar.com.educacionit.services.ArticulosServices;
+//import ar.com.educacionit.services.exceptions.ServiceException;
+//import ar.com.educacionit.services.impl.ArticulosServicesImpl;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
+//import jakarta.ws.rs.core.Response.Status;
 
 
 @Path("productos")
@@ -20,7 +22,6 @@ public class ArticuloResource {
 	
 	/*
 	 * retorna json
-	*/
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAll() {
@@ -28,8 +29,19 @@ public class ArticuloResource {
 		try {
 			Collection<Articulos> articulos = articuloService.findAll();
 			return Response.ok(articulos).build();
+			//por medio de jackson se convierte a json 
 		} catch (ServiceException e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 		}
+	}
+	*/
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response find() {
+		//ArticulosServices articuloService = new ArticulosServicesImpl();
+		Map<String,String> datos = new HashMap<>();
+		datos.put("clave1", "valor1");
+		datos.put("clave2", "valor2");
+		return Response.ok(datos).build();
 	}
 }
